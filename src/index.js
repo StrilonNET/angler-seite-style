121801d860361e6d48c92821acd0df07c1f06b9b
GameWithJerry <65787801+GameWithJerry@users.noreply.github.com>
2021-09-13 17:37:28 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-require('./anglergames.scss');
+require('./bulma.scss');
+require('./anglergames.css');
+require('./twemoji.css');
+require('./ckeditor.css');
+require('@fortawesome/fontawesome-free/css/all.css');
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,54 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle.js",
   },
   module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: 'css-loader'
+            loader: "css-loader",
           },
           {
-            loader: 'sass-loader',
+            loader: "sass-loader",
             options: {
               sourceMap: true,
               // options...
-            }
-          }
-        ]
-    }]
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'css/anglergames.css'
+      filename: "anglergames.css",
     }),
-  ]
+  ],
 };
